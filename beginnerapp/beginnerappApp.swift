6b1026854b7714371eef6c8e872313a6e8815bf7
Raj Shah <free3ntertainment@rajs-air.lan>
2024-07-31 12:14:07 -0700

    init

---
+++ beginnerapp/beginnerappApp.swift
@@ -0,0 +1,17 @@
+//
+//  beginnerappApp.swift
+//  beginnerapp
+//
+//  Created by Raj Shah on 7/22/24.
+//
+
+import SwiftUI
+
+@main
+struct beginnerappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
